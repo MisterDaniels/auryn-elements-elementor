39a9fadd12fb3435d13cdde035864629319f4a3c
Daniel Silva <dodosilva51@gmail.com>
2020-04-05 17:17:15 -0300

    Created the structure of the plugin and the first use of Elementor widget

---
+++ AurynElements.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AurynElements;
+
+use AurynCategories;
+
+/**
+ * Plugin Name: Auryn Custom Elements
+ * Description: Custom elements for Auryn
+ * Plugin URI: https://www.auryn.com.br
+ * Version: 0.0.1
+ * Author: Auryn
+ * Author URI: https://www.auryn.com.br
+ * Text Domain: auryn-elements
+ */
+
+if (!defined('ABSPATH')) exit;
+
+class AurynElements {
+
+    private static $instance = null;
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    public function init() {
+        add_action('elementor/elements/categories_registered', array(
+            $this, 'AddCustomCategories'));
+
+        add_action('elementor/widgets/widgets_registered', array(
+            $this, 'registerAurynWidgets'));
+    }
+
+    function AddCustomCategories($elementsManager) {
+        $elementsManager->add_category(
+            'auryn-category',
+            [
+                'title' => __('Auryn Elements', 'auryn-elements'),
+                'icon' => 'fa fa-plug'
+            ]
+        );
+    }
+
+    public function registerAurynWidgets() {
+        if (defined('ELEMENTOR_PATH') && class_exists('\Elementor\Widget_Base')) {
+            $templateFile = plugin_dir_path(__FILE__).'widgets/title-widget/auryn-title-widget.php';
+
+            if ($templateFile && is_readable($templateFile)) {
+                require_once $templateFile;
+            }
+        }
+    }
+
+}
+
+AurynElements::getInstance()->init();
