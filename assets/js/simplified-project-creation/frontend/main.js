a36fc3f9af112781efac50a82045d5ea9ec6297a
Daniel Silva <dodosilva51@gmail.com>
2020-12-10 12:04:30 -0300

    Removido espaços da string #1

--- assets/js/simplified-project-creation/frontend/main.js
+++ assets/js/simplified-project-creation/frontend/main.js
@@ -59,8 +59,7 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
             const selectElement = e.target;
             const selectedBookSize = jQuery(selectElement).find(':selected').val();
 
-            jQuery(buttonRedirectElement).attr('href', `https://${bookSizesData.company}/fotolivros/
-                createSimpleProject?bookSize=${ selectedBookSize }`);
+            jQuery(buttonRedirectElement).attr('href', `https://${bookSizesData.company}/fotolivros/createSimpleProject?bookSize=${ selectedBookSize }`);
             jQuery(buttonRedirectElement).attr('bookSize', selectedBookSize);
         });
 
